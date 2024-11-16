2168f999e3a41d02db75c6f2cda68db59c9dcca5
captainnusang <111036604+captainnusang@users.noreply.github.com>
2024-11-16 18:12:04 +0700

    Update Cl V2.lua

--- Cl V2.lua
+++ Cl V2.lua
@@ -87,7 +87,7 @@ local Tp = true
 
 ASection:NewToggle("Auto OrbTP", "Teleports you to orbs automatically (portal relic)", function(State)
     Tp = State
-spawn(function()
+task.spawn(function()
     while Tp and wait() do
 	local MyFol
         local CombatFolder = workspace:FindFirstChild("CombatFolder")
@@ -100,8 +100,7 @@ spawn(function()
             pcall(function()
             for i,v in pairs(MyFol) do
                 if v:IsA("BasePart") then
-                v.CFrame = HRP.CFrame + Vector3.new(0, 0.1, 0)
-		HRP.CFrame = v.CFrame + Vector3.new(0, 0.1, 0)
+                v.CFrame = HRP.CFrame
                 end
         end
             end)
